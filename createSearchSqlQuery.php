995472e95e546ea4b778c97f5904853a45b57799
Caner Gedik <130908718+canergedik28@users.noreply.github.com>
2026-02-28 16:49:47 +0300

    Add createSearchSqlQuery class for SQL query generation

---
+++ createSearchSqlQuery.php
@@ -0,0 +1,53 @@
+<?php
+
+class createSearchSqlQuery {
+
+    private $test;
+
+    private $test1;
+
+    private $test2;
+
+    private $test3;
+
+    private $test4;
+
+    private $page;
+
+    private static $pageSize = 20;
+
+    private $createSearchSqlQuery;
+
+   public function createSearchQuery(): string{
+        $this->createSearchSqlQuery =  "SELECT {column} from test  as tm
+        LEFT JOIN test1 as ah ON test1.testID = test.test_id";
+        if($this->test && $this->test != ''){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test="'.$this->test.'"';
+        } 
+        if($this->test1){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test1="'.$this->test1.'"';
+        } 
+        if($this->test2){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test2="'.$this->test2.'"';
+        } 
+
+        if($this->test3){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test3="'.$this->test2.'"';
+        } 
+
+        if($this->test4){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test4="'.$this->test2.'"';
+        } 
+        
+        if($this->page){
+            $this->createSearchSqlQuery .=   " LIMIT ".$this->pageSize." OFFSET ".($this->page == 1 ? "0" : $this->pageSize*($this->page-1));
+        }
+        return $this->createSearchSqlQuery;
+      }
+   
+}
