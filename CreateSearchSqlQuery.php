af4653ff4a01961ff8ae55bc64cc060d8d91689e
Caner Gedik <130908718+canergedik28@users.noreply.github.com>
2026-02-28 20:42:11 +0300

    Update CreateSearchSqlQuery.php

--- CreateSearchSqlQuery.php
+++ CreateSearchSqlQuery.php
@@ -12,6 +12,10 @@ class CreateSearchSqlQuery {
 
     private $test4;
 
+    private $price_min;
+
+    private $price_max;
+
     private $page;
 
     private $order = 2;
@@ -45,6 +49,20 @@ class CreateSearchSqlQuery {
           $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
           $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' test4="'.$this->test4.'"';
         } 
+
+
+        if($this->price_min){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' price >='.$this->price_min.'';
+        } 
+
+        if($this->price_max){
+          $this->createSearchSqlQuery =  preg_match('/WHERE/',$this->createSearchSqlQuery)   ? $this->createSearchSqlQuery : $this->createSearchSqlQuery .= ' WHERE';
+          $this->createSearchSqlQuery .=  (substr($this->createSearchSqlQuery,-5,  null)  == "WHERE" ? ' ' : ' AND '). ' price <='.$this->price_max.'';
+        } 
+
+
+       
         if($this->order){
           switch($this->order){
              case 1 : $this->createSearchSqlQuery .= ' ORDER BY created_date ASC ';  break;
